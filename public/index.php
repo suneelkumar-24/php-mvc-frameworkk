ce80243439318b447f21a6d398b052b0377ac1da
Safee's Team PC <k163997@nu.edu.pk>
2022-05-06 20:49:50 +0500

    1

--- public/index.php
+++ public/index.php
@@ -18,6 +18,8 @@ $app->router->get('/login',[AuthController::class,'login']);
 $app->router->post('/login',[AuthController::class,'login']);
 $app->router->get('/register',[AuthController::class,'register']);
 $app->router->post('/register',[AuthController::class,'register']);
+$app->router->get('/create',[AuthController::class,'create']);
+
 
 
 
